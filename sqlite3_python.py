43fc16b559186a5b9a2c5c47541d1f762a988952
Gusmatto <gustavo.matto46@gmail.com>
2017-05-04 12:21:43 -0300

    Conexión de base de datos con Python.

--- sqlite3_python.py
+++ sqlite3_python.py
@@ -4,4 +4,6 @@ base = sqlite3.connect('/home/gusmatoo/informatica.db')
 c = base.cursor()
 
 c.execute('SELECT * FROM Fabricantes;')
-print(c)
+
+for columna in c.execute('SELECT * FROM Articulos ORDER BY Nombre;'):
+    print(columna)
