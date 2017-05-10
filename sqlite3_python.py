88cab9e5f6defe9f6393e45577edc81371f3df5e
Gusmatto <gustavo.matto46@gmail.com>
2017-05-10 09:38:47 -0300

    Conexión de base de datos con Python. Consultas a través de "input". Agregué ingreso de articulos con nombre, precio y fabricante.

--- sqlite3_python.py
+++ sqlite3_python.py
@@ -8,37 +8,48 @@ print("1 - Ver lista de fabricantes")
 print("2 - Ver lista de articulos")
 print("3 - Ingresar fabricante")
 print("4 - Ver lista articulos+fabricantes")
+print("5 - Ingresar articulo, precio y fabricante")
 print("")
 
-opc = {1:"FABRICANTES",
-      2:"ARTICULOS"}
+opc = {1: "FABRICANTES",
+      2: "ARTICULOS"}
 
 opcion = int(input("Ingrese opción: "))
 if opcion == 4:
-   c.execute( '''SELECT ARTICULOS.ID, ARTICULOS.NOMBRE, ARTICULOS.PRECIO, FABRICANTES.NOMBRE
+   c.execute('''SELECT ARTICULOS.ID, ARTICULOS.NOMBRE, ARTICULOS.PRECIO, FABRICANTES.NOMBRE
    FROM ARTICULOS INNER JOIN FABRICANTES ON FABRICANTES.ID = ARTICULOS.FAB''')
    a = c.fetchall()
-   print("ID   NOMBRE  PRECIO  FABRICANTE")
+   print("ID NOMBRE PRECIO FABRICANTE")
    for i in a:
        print(i[0], i[1], i[2], i[3])
+
 if opcion < 3:
    c.execute('SELECT * FROM {};'.format(opc[opcion]))
 
 if opcion == 3:
-   nombre = input("Ingrese nombre")
+   nombre = input("Ingrese nombre: ")
    c.execute('INSERT INTO FABRICANTES(NOMBRE) VALUES("{}");'.format(nombre))
    base.commit()
+
+if opcion == 5:
+    articulo = input("Ingrese articulo: ")
+    precio = input("Ingrese precio: ")
+    id_fab = input("Ingrese id de fabricante: ")
+    c.execute('INSERT INTO ARTICULOS VALUES(?, ?, ?)', (articulo, precio, id_fab))
 
 if opcion != 3:
    a = c.fetchall()
    if opcion == 1:
-       print("ID   NOMBRE")
+       print("ID NOMBRE")
        for i in a:
            print(i[0], i[1])
+
    if opcion == 2:
-       print("ID   NOMBRE  PRECIO  ID_FAB")
+       print("ID NOMBRE PRECIO ID_FAB")
        for i in a:
            print(i[0], i[1], i[2], i[3])
 
 
+   base.commit()
+   base.close()
 
